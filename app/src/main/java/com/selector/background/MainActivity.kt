4f9f7e44aaf3c20f2246fd192d01d6482d315253
hrshin_me <hrshin_me@daum.net>
2024-07-28 07:32:34 +0900

    초기 UI 생성

---
+++ app/src/main/java/com/selector/background/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.selector.background
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.selector.background.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
